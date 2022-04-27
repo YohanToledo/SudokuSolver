d5918cc52267651dbb83ae8cdc2c2cf855ef3e93
YohanToledo <yohan.gabriel.bernart@gmail.com>
2022-04-27 01:27:46 -0300

    solveBoard method using recursive function

--- src/sudoku_solver/Application.java
+++ src/sudoku_solver/Application.java
@@ -7,24 +7,32 @@ public class Application {
 	public static void main(String[] args) {
 		
 		int[][] board = {
-				{7,0,2,1,5,0,6,0,0},
-				{0,0,0,0,0,3,0,0,0},
-				{1,0,0,0,0,9,5,0,0},
-				{8,0,0,0,0,0,0,9,0},
-				{0,4,3,0,0,0,7,5,0},
-				{0,9,0,0,0,0,0,0,8},
-				{0,0,9,7,0,0,0,0,5},
-				{0,0,0,2,0,0,0,0,0},
-				{0,0,7,0,4,0,2,0,3}
+				{7, 0, 2, 0, 5, 0, 6, 0, 0},
+				{0, 0, 0, 0, 0, 3, 0, 0, 0},
+				{1, 0, 0, 0, 0, 9, 5, 0, 0},
+				{8, 0, 0, 0, 0, 0, 0, 9, 0},
+				{0, 4, 3, 0, 0, 0, 7, 5, 0},
+				{0, 9, 0, 0, 0, 0, 0, 0, 8},
+				{0, 0, 9, 7, 0, 0, 0, 0, 5},
+				{0, 0, 0, 2, 0, 0, 0, 0, 0},
+				{0, 0, 7, 0, 4, 0, 2, 0, 3}
 			};
 		
 		printBoard(board);
-		System.out.println(isNumberInRow(board, 7, 1));
-		System.out.println(isNumberInColumn(board, 3, 4));
-		System.out.println(isNumberInBox(board, 7, 1, 1));
+		
+		if(solveBoard(board)) {
+			System.out.println();
+			System.out.println("======== Solved Sudoku ========");
+			System.out.println();
+			printBoard(board);
+		}
+		else {
+			System.out.println("Unable to solve this game! :(");
+		}		
 	}
 	
 	
+		
 	public static boolean isNumberInRow(int[][] board, int number, int row) {
 		for(int i = 0; i < GRID_SIZE; i++) {
 			if(number == board[row][i]) {
@@ -64,6 +72,30 @@ public class Application {
 		return !isNumberInRow(board, number, row) &&
 				!isNumberInColumn(board, number, column) &&
 				!isNumberInBox(board, number, row, column);
+	}
+	
+	
+	public static boolean solveBoard(int[][] board) {
+		for(int row = 0; row < GRID_SIZE; row++) {
+			for(int column = 0; column < GRID_SIZE; column++) {
+				if(board[row][column] == 0) {
+					for(int numberToTry = 1; numberToTry <= GRID_SIZE; numberToTry++ ) {
+						if(isValidPosition(board, numberToTry, row, column)) {
+							board[row][column] = numberToTry;
+							
+							if(solveBoard(board)) {
+								return true;
+							}
+							else { //if the recursive call return false, the last position is set back to zero and the application tries a different number
+								board[row][column] = 0;
+							}
+						}
+					}
+					return false; // if the for loop test all possible number and nothing is valid, it returns false and the application go back to the last position to try a different combination of numbers to solve the board
+				}
+			}
+		}
+		return true;
 	}
 	
 	
